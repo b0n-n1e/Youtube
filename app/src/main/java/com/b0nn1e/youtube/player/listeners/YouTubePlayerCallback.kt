8d0fe7474fa3134c758a154050389409949aae8b
B0nn1e <3110901614@qq.com>
2025-09-09 20:28:19 +0800

    chore(player) : copy by {Pierfrancesco Soffritti} github:https://github.com/PierfrancescoSoffritti/android-youtube-player/tree/dev/core

---
+++ app/src/main/java/com/b0nn1e/youtube/player/listeners/YouTubePlayerCallback.kt
@@ -0,0 +1,7 @@
+package com.b0nn1e.youtube.player.listeners
+
+import com.b0nn1e.youtube.player.YouTubePlayer
+
+interface YouTubePlayerCallback {
+    fun onYouTubePlayer(youTubePlayer: YouTubePlayer)
+}
