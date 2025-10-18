3ed6e965fab8752f1223fbbd2c0cc1eb7882a660
B0nn1e <3110901614@qq.com>
2025-10-18 18:49:02 +0800

    feat(videoPlayer): simple video playback implement

--- app/src/main/java/com/b0nn1e/youtube/video/VideoPlayerScreen.kt
+++ app/src/main/java/com/b0nn1e/youtube/video/VideoPlayerScreen.kt
@@ -1,12 +1,55 @@
 package com.b0nn1e.youtube.video
 
-import androidx.compose.material3.Text
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
+import com.b0nn1e.youtube.player.PlayerConstants.PlayerError
+import com.b0nn1e.youtube.player.YouTubePlayer
+import com.b0nn1e.youtube.player.listeners.AbstractYouTubePlayerListener
+import com.b0nn1e.youtube.player.views.YouTubePlayerView
 
 @Composable
 fun VideoPlayerScreen(
     videoId : String
 ) {
-    Text(videoId)
 
+    var youTubePlayer by remember { mutableStateOf<YouTubePlayer?>(null) }
+    Column {
+        AndroidView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp), // 设置固定高度
+            factory = { context ->
+                YouTubePlayerView(context).apply {
+                    (context as? LifecycleOwner)?.lifecycle?.addObserver(this)
+                    //取消自动初始化，以供动态加载指定的videoId
+                    enableAutomaticInitialization = false
+                    initialize(object : AbstractYouTubePlayerListener() {
+                        override fun onReady(player: YouTubePlayer) {
+                            youTubePlayer = player
+                            player.loadVideo(videoId, 0f)
+                        }
+
+                        override fun onError(player: YouTubePlayer, error: PlayerError) {
+                            Log.e("YouTubePlayer", "Error: $error")
+                        }
+                    })
+                }
+            },
+            update = { youTubePlayerView ->
+
+            }
+        )
+    }
 }
--- app/src/main/java/com/b0nn1e/youtube/video/VideoPlayerViewModel.kt
+++ app/src/main/java/com/b0nn1e/youtube/video/VideoPlayerViewModel.kt
@@ -1,8 +1,30 @@
 package com.b0nn1e.youtube.video
 
 import androidx.lifecycle.ViewModel
+import com.b0nn1e.youtube.player.YouTubePlayer
+import com.b0nn1e.youtube.player.listeners.AbstractYouTubePlayerListener
+import com.b0nn1e.youtube.player.views.YouTubePlayerView
+
 
 class VideoPlayerViewModel: ViewModel() {
 
     val model by lazy { VideoPlayerModel() }
+
+    //由viewModel来控制播放器的行为
+    private var youTubePlayer : YouTubePlayer? = null
+
+
+    val playerListener = object : AbstractYouTubePlayerListener() {
+        override fun onReady(youTubePlayer: YouTubePlayer) {
+            this@VideoPlayerViewModel.youTubePlayer = youTubePlayer
+        }
+    }
+
+    fun initPlayer(
+        youTubePlayerView: YouTubePlayerView,
+        videoId : String
+    ){
+        youTubePlayerView.enableAutomaticInitialization = false
+
+    }
 }
