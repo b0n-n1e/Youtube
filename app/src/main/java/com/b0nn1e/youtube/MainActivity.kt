240ecc37ce176537b2198deeb75e7dc16bb4fd3e
B0nn1e <3110901614@qq.com>
2025-10-20 00:07:30 +0800

    feat(home): refresh menu of topAppbar

--- app/src/main/java/com/b0nn1e/youtube/MainActivity.kt
+++ app/src/main/java/com/b0nn1e/youtube/MainActivity.kt
@@ -1,28 +1,43 @@
 package com.b0nn1e.youtube
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.b0nn1e.youtube.home.HomeScreen
 import com.b0nn1e.youtube.home.HomeViewModel
 import com.b0nn1e.youtube.mine.MineScreen
 import com.b0nn1e.youtube.search.SearchScreen
 import com.b0nn1e.youtube.search.SearchViewModel
-import com.b0nn1e.youtube.ui.theme.YoutubeTheme
 import com.b0nn1e.youtube.ui.widget.BottomBarWidget
 import com.b0nn1e.youtube.ui.widget.Screen
 import com.b0nn1e.youtube.ui.widget.list
@@ -44,6 +59,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 @Composable
 fun CenterScreen(
     homeViewModel: HomeViewModel,
@@ -52,8 +69,23 @@ fun CenterScreen(
     val navController = rememberNavController()
     val items = list
 
+    //获取当前路由状态，用于控制 TopAppBar 的内容
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "YouTube") },
+                //在 actions slot 中添加菜单按钮
+                actions = {
+                    if (currentRoute == Screen.Home.route) HomeRefreshMenu(homeViewModel)
+                    if (currentRoute == Screen.Search.route) HomeRefreshMenu(searchViewModel)
+
+                }
+            )
+        },
         bottomBar = {
             BottomBarWidget(
                 navController = navController,
@@ -86,12 +118,56 @@ fun CenterScreen(
             composable(Screen.Mine.route) { MineScreen() }
             composable(
                 route = Screen.VideoPlayer.route,
-                arguments = listOf(navArgument("videoId") { type = NavType.StringType })
+                arguments = listOf(navArgument("videoId") { type = NavType.StringType }),
+                deepLinks = listOf(
+                    navDeepLink {
+                        // 匹配标准长链接
+                        uriPattern = "https://www.youtube.com/watch?v={videoId}"
+                        action = Intent.ACTION_VIEW
+                    },
+                    navDeepLink {
+                        uriPattern = "http://www.youtube.com/watch?v={videoId}"
+                        action = Intent.ACTION_VIEW
+                    })
             ) {navBackStateEntry ->
                 val videoId = navBackStateEntry.arguments?.getString("videoId")?:""
                 VideoPlayerScreen(videoId)
             }
         }
+    }
+}
+
+/**
+ * 用于 Home 界面的刷新菜单 Composable
+ */
+@Composable
+fun HomeRefreshMenu(viewModel: ViewModel) {
+    var isMenuExpanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { isMenuExpanded = true }) {
+        Icon(Icons.Filled.MoreVert, contentDescription = "更多选项")
+    }
+
+    DropdownMenu(
+        expanded = isMenuExpanded,
+        onDismissRequest = { isMenuExpanded = false }
+    ) {
+        DropdownMenuItem(
+            text = { Text("刷新") },
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Refresh,
+                    contentDescription = "刷新图标"
+                )
+            },
+            onClick = {
+                isMenuExpanded = false
+                if(viewModel is HomeViewModel){
+                    viewModel.getPopularVideos()
+                }
+            }
+        )
+        // 可以在这里添加其他菜单项，例如：设置
     }
 }
 
